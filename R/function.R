da303680548aa056f2cdb342472a8f5df1ca382b
maggie <mwalters@imperial.ac.uk>
2022-06-21 15:41:01 +0100

    documentation added

--- R/function.R
+++ R/function.R
@@ -1,3 +1,13 @@
+##' Add two numbers
+##'
+##' @param a The first number
+##' @param b The second number
+##'
+##' @return Sum of the two numbers
+##' @export
+##'
+##' @examples
+##' a = 1; b = 2; test_function(1,2)
 test_function <- function(a, b){
   c <- a+b
   return(c)
