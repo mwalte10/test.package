c4812942d8caaf4e02b24fbbcf5348e71304715d
maggie <mwalters@imperial.ac.uk>
2022-06-22 11:57:57 +0100

    added initial set up under a package_setup folder

---
+++ package_setup/setup.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/mwalters/Documents/r_packages/")
+# install.packages("usethis")
+# install.packages("Rtools")
+#always qualify packages
+usethis::create_package("test.package")
+#changes the license, then can change the copyright owner to Imperial
+usethis::use_mit_license()
+#ctl shift l gives devtools::load_all
